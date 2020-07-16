e876b879ef65ac62fc237bb6fa6bdc039c9bc746
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2020-07-16 17:24:19 +0800

    update

--- model/lstm.py
+++ model/lstm.py
@@ -1,43 +1,55 @@
 import os
-import json
-import keras
-from tf.keras.layers import Bidirectional, LSTM, Dropout, Dense
-from tf.keras.models import Sequential
-from global_settings import CONFIG_FOLDER
+from global_settings import DATA_FOLDER
+from tensorflow.keras.layers import GRU, Dropout, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import TensorBoard
 from tools.data_tools import cat_generator, cat_loader
+from datetime import datetime
 
 from config.model_confg import rnn_config
 
-rnn_config
-
-
-def run_lstm(model, dataset):
-
-    model.fit(
-        x=None, y=None, epochs=1, verbose=2, callbacks=None,
-        max_queue_size=10, workers=1, use_multiprocessing=True
-    )
+generator = rnn_config['generator']
+rnn_num, rnn_dim = rnn_config['rnn_num'], rnn_config['rnn_dim']
+dnn_num, drop = rnn_config['dnn_num'], rnn_config['drop']
+epochs, batch_size = rnn_config['epochs'], rnn_config['batch_size']
 
 
 def lstm(X_train, y_train):
     model = Sequential()
-    model.add(Bidirectional(LSTM(units=128, input_shape=[None, X_train.shape[2]])))
-    model.add(Dropout(rate=0.5))
-    model.add(Dense(units=128, activation='relu'))
+    model.add(GRU(units=rnn_dim, input_shape=[None, X_train.shape[2]]))
+
+    for _ in range(rnn_num-1):
+        model.add(GRU(units=rnn_dim))
+
+    for _ in range(dnn_num):
+        model.add(Dense(units=rnn_dim*2, activation='relu'))
+        model.add(Dropout(rate=drop))
+
     model.add(Dense(y_train.shape[1], activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
     return model
 
 
+def train_lstm(model, dataset):
+    NAME = '-'.join([f'rnn_num_{rnn_num}', f'rnn_dim_{rnn_dim}', f'dnn_num_{dnn_num}'])
+    log_dir = os.path.join(DATA_FOLDER, f'{dataset}_log')
+    if not os.path.isdir(log_dir): os.mkdir(log_dir)
+    log_path = os.path.join(log_dir, '-'.join([NAME, datetime.now().strftime("%Y%m%d-%H%M%S")]))
+    callbacks = TensorBoard(log_dir=log_path)
 
-model = Sequential()
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.add(LSTM(32, return_sequences=True, input_shape=(None, 5)))
-model.add(LSTM(8, return_sequences=True))
-model.add(TimeDistributed(Dense(2, activation='sigmoid')))
+    model.fit(
+        x=None, y=None, batch_size=batch_size, epochs=1, verbose=0,
+        validation_data=(None, None), callbacks=[callbacks],
+        max_queue_size=10, workers=5, use_multiprocessing=True
+    )
 
-print(model.summary(90))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer='adam')
+def test_lstm(trainX, trainy, testX, testy):
+    verbose, epochs, batch_size = 0, 15, 64
+    n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+    model = Sequential()
+    _, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+
+    return accuracy
--- tools/data_tools.py
+++ tools/data_tools.py
@@ -4,35 +4,35 @@ import pandas as pd
 from global_settings import DATA_FOLDER
 from config.data_config import data_config
 
-w, s, batch_size = data_config['w'], data_config['s'], data_config['batch_size']
+w, s, max_len, batch = data_config['w'], data_config['s'], data_config['max_len'], data_config['batch']
 
 
 def cat_generator(dataset):
     assert dataset.split('_')[0] in ['ASAS', 'MACHO', 'Synthesis'], 'invalid dataset name'
     catalog = pd.read_csv(os.path.join(DATA_FOLDER, dataset, 'catalog.csv'), index_col=0)
-    for i in range(0, np.shape(catalog)[0], batch_size):
-        paths, cats = list(catalog['Path'])[i:i+batch_size], list(catalog['Class'])[i:i+batch_size]
-        X, y = load_Xy(dataset, cats, paths)
+    for i in range(0, np.shape(catalog)[0], batch):
+        paths, cats = list(catalog['Path'])[i:i + batch], list(catalog['Class'])[i:i + batch]
+        x, y = load_xy(dataset, cats, paths)
 
-        yield np.array(X), np.array(y)
+        yield np.array(x), np.array(y)
 
 
 def cat_loader(dataset):
     assert dataset.split('_')[0] in ['ASAS', 'MACHO', 'Synthesis'], 'invalid dataset name'
     catalog = pd.read_csv(os.path.join(DATA_FOLDER, {dataset}, 'catalog.csv'), index_col=0)
     paths, cats = list(catalog['Path']), list(catalog['Class'])
-    X, y = load_Xy(dataset, cats, paths)
+    x, y = load_xy(dataset, cats, paths)
 
-    return np.array(X), np.array(y)
+    return np.array(x), np.array(y)
 
 
-def load_Xy(dataset, cats, paths):
-    X, y = [], []
+def load_xy(dataset, cats, paths):
+    x, y = [], []
     for cat, path in list(zip(cats, paths)):
         df = pd.read_csv(os.path.join(DATA_FOLDER, dataset, path))
-        X.append(processing(df)); y.append([cat])  # TODO: Filter DataFrames shorter than 80
+        x.append(processing(df)); y.append([cat])  # TODO: Filter DataFrames shorter than 80
 
-    return X, y
+    return x, y
 
 
 def processing(df):
@@ -43,6 +43,7 @@ def processing(df):
     dtdm_bin = np.array([], dtype=np.int64).reshape(0, 2 * w)
     for i in range(0, np.shape(dtdm_org)[0] - (w - 1), s):
         dtdm_bin = np.vstack([dtdm_bin, dtdm_org[i: i + w, :].reshape(-1)])
+        if np.shape(dtdm_bin)[0] == max_len: break
 
     return dtdm_bin
 
