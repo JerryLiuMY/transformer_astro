b248100c4c59e291de136ec5ddbb1c25f23076dd
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2020-08-10 23:33:04 +0800

    Update exec_config.py

--- config/exec_config.py
+++ config/exec_config.py
@@ -4,29 +4,30 @@ import tensorflow as tf
 import subprocess
 import os
 
-tpu_devices = tf.config.list_logical_devices('TPU')
 gpu_devices = tf.config.list_physical_devices('GPU')
 cpu_devices = tf.config.list_physical_devices('CPU')
 
-if bool(len(tpu_devices)):
-    print("Number of TPU devices: ", len(tpu_devices))
+try:
     resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
     tf.config.experimental_connect_to_cluster(resolver)
     tf.tpu.experimental.initialize_tpu_system(resolver)
+    tpu_devices = tf.config.list_logical_devices('TPU')
+    print("Number of TPU devices: ", len(tpu_devices))
     strategy = tf.distribute.TPUStrategy(resolver)
-elif bool(len(gpu_devices)):
-    print("Number of GPU devices: ", len(gpu_devices))
-    subprocess.run("nvidia-smi")
-    strategy = tf.distribute.MirroredStrategy()
-elif bool(len(cpu_devices)):
-    print("Number of CPU devices: ", len(gpu_devices))
-    if FLAG in ['colab', 'floyd']:
-        subprocess.run("cat /proc/cpuinfo  | grep 'name'| uniq")
+except KeyError:
+    if bool(len(gpu_devices)):
+        print("Number of GPU devices: ", len(gpu_devices))
+        subprocess.run("nvidia-smi", stdout=subprocess.PIPE, shell=True)
+        strategy = tf.distribute.MirroredStrategy()
+    elif bool(len(cpu_devices)):
+        print("Number of CPU devices: ", len(cpu_devices))
+        if FLAG in ['colab', 'floyd']:
+            subprocess.Popen("cat /proc/cpuinfo | grep name | uniq", stdout=subprocess.PIPE, shell=True)
+        else:
+            subprocess.Popen("sysctl -n machdep.cpu.brand_string", stdout=subprocess.PIPE, shell=True)
+        strategy = tf.distribute.get_strategy()
     else:
-        subprocess.run("sysctl -n machdep.cpu.brand_string")
-    strategy = tf.distribute.get_strategy()
-else:
-    raise EnvironmentError("No physical devices or remote devices")
+        raise EnvironmentError("No physical devices or remote devices")
 
 
 metrics = [
