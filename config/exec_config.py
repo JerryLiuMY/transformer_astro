e2500e902be3e390df3ed1cb1ccec8d38a96d2f8
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2020-08-15 13:24:28 +0800

    Update exec_config.py

--- config/exec_config.py
+++ config/exec_config.py
@@ -1,30 +1,32 @@
 from global_settings import FLAG
 import tensorflow as tf
 import subprocess
+import os
 
 gpu_devices = tf.config.list_physical_devices('GPU')
 cpu_devices = tf.config.list_physical_devices('CPU')
 
-# resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
-# tf.config.experimental_connect_to_cluster(resolver)
-# tf.tpu.experimental.initialize_tpu_system(resolver)
-# tpu_devices = tf.config.list_logical_devices('TPU')
-# print("Number of TPU devices: ", len(tpu_devices))
-# strategy = tf.distribute.TPUStrategy(resolver)
-
-if bool(len(gpu_devices)):
-    print("Number of GPU devices: ", len(gpu_devices))
-    subprocess.run("nvidia-smi", stdout=subprocess.PIPE, shell=True)
-    strategy = tf.distribute.MirroredStrategy()
-elif bool(len(cpu_devices)):
-    print("Number of CPU devices: ", len(cpu_devices))
-    if FLAG in ['colab', 'floyd']:
-        subprocess.Popen("cat /proc/cpuinfo | grep name | uniq", stdout=subprocess.PIPE, shell=True)
+try:
+    resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='grpc://' + os.environ['COLAB_TPU_ADDR'])
+    tf.config.experimental_connect_to_cluster(resolver)
+    tf.tpu.experimental.initialize_tpu_system(resolver)
+    tpu_devices = tf.config.list_logical_devices('TPU')
+    print("Number of TPU devices: ", len(tpu_devices))
+    strategy = tf.distribute.TPUStrategy(resolver)
+except KeyError:
+    if bool(len(gpu_devices)):
+        print("Number of GPU devices: ", len(gpu_devices))
+        subprocess.run("nvidia-smi", stdout=subprocess.PIPE, shell=True)
+        strategy = tf.distribute.MirroredStrategy()
+    elif bool(len(cpu_devices)):
+        print("Number of CPU devices: ", len(cpu_devices))
+        if FLAG in ['colab', 'floyd']:
+            subprocess.Popen("cat /proc/cpuinfo | grep name | uniq", stdout=subprocess.PIPE, shell=True)
+        else:
+            subprocess.Popen("sysctl -n machdep.cpu.brand_string", stdout=subprocess.PIPE, shell=True)
+        strategy = tf.distribute.get_strategy()
     else:
-        subprocess.Popen("sysctl -n machdep.cpu.brand_string", stdout=subprocess.PIPE, shell=True)
-    strategy = tf.distribute.get_strategy()
-else:
-    raise EnvironmentError("No physical devices or remote devices")
+        raise EnvironmentError("No physical devices or remote devices")
 
 
 train_config = {
