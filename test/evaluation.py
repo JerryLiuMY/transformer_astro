733fb0dce6dabcd0b5c49a403da61a916a4de421
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2020-08-30 23:50:26 +0800

    Update evaluation.py

--- test/evaluation.py
+++ test/evaluation.py
@@ -1,31 +1,34 @@
 import os
 import json
 import numpy as np
+import pandas as pd
 import tensorflow as tf
+from data.loader import token_loader
 from tensorflow.python.keras import Model
 from tools.log_tools import plot_confusion
 from tools.dir_tools import create_dirs
+from data.loader import data_loader
 
 
 def evaluate(exp):
-    y_evalu, y_predi_seq = predict(exp)
-    y_evalu, y_predi = y_evalu, y_predi_seq[:, -1, :]
+    true, pred_seq = predict(exp)
+    true, pred = true, pred_seq[:, -1, :]
     exp_dir, exp_name = exp.exp_dir, exp.exp_name
     eva_dir = os.path.join(exp_dir, 'test'); create_dirs(eva_dir)
     eva_path = os.path.join(eva_dir, exp_name); create_dirs(eva_path)
 
-    y_evalu_spar = exp.encoder.inverse_transform(y_evalu)
-    y_predi_spar = exp.encoder.inverse_transform(y_predi)
-    confusion_fig = plot_confusion(y_evalu_spar, y_predi_spar, categories=exp.categories)
+    true_spar = exp.encoder.inverse_transform(true)
+    pred_spar = exp.encoder.inverse_transform(pred)
+    confusion_fig = plot_confusion(true_spar, pred_spar, categories=exp.categories)
     confusion_fig.savefig(os.path.join(eva_path, 'confusion.pdf'), bbox_inches='tight')
 
     ccl = tf.keras.losses.CategoricalCrossentropy()
-    ccm = tf.keras.metrics.CategoricalAccuracy(); ccm.update_state(y_evalu, y_predi)
-    pre = tf.keras.metrics.Precision(); pre.update_state(y_evalu, y_predi)
-    rec = tf.keras.metrics.Recall(); rec.update_state(y_evalu, y_predi)
+    ccm = tf.keras.metrics.CategoricalAccuracy(); ccm.update_state(true, pred)
+    pre = tf.keras.metrics.Precision(); pre.update_state(true, pred)
+    rec = tf.keras.metrics.Recall(); rec.update_state(true, pred)
 
     summary = {
-        "categorical_crossentropy_loss": round(float(ccl(y_evalu, y_predi).numpy()), 3),
+        "categorical_crossentropy_loss": round(float(ccl(true, pred).numpy()), 3),
         "categorical_accuracy": round(float(ccm.result().numpy()), 3),
         "precision": round(float(pre.result().numpy()), 3),
         "recall": round(float(rec.result().numpy()), 3)
@@ -36,17 +39,28 @@ def evaluate(exp):
 
 
 def predict(exp):
+    token, dataset_evalu = token_loader(exp.dataset_name), data_loader(exp.dataset_name, 'evalu')
     with tf.device('/CPU:0'):
+        # generate prediction
         model = Model(inputs=exp.model.inputs, outputs=exp.model.get_layer('softmax').output)
-        y_evalu = np.array([]).reshape(0, len(exp.categories))
-        for x_evalu_, y_evalu_ in exp.dataset_evalu.take(-1):
+        y_evalu = np.array([]).reshape((0, len(exp.categories)))
+        for x_evalu_, y_evalu_ in dataset_evalu.take(-1):
             y_evalu = np.vstack([y_evalu, y_evalu_.numpy()])
-        y_somax_seq = model.predict(exp.dataset_evalu)
-        y_predi_seq = np.zeros(shape=np.shape(y_somax_seq))
+        y_somax_seq = model.predict(dataset_evalu)
 
-        for t in range(np.shape(y_somax_seq)[1]):
-            max_arg = tf.math.argmax(y_somax_seq[:, t, :], axis=1)
+        # group prediction
+        true = np.array([]).reshape((0, len(exp.categories)))
+        soft_seq = np.array([]).reshape((0, np.shape(y_somax_seq)[1], np.shape(y_somax_seq)[2]))
+        for tok in pd.unique(token):
+            index = np.array([idx for idx, foo in enumerate(token) if foo == tok])
+            true = np.vstack([true, np.mean(y_evalu[index], axis=0, keepdims=True)])
+            soft_seq = np.vstack([soft_seq, np.mean(y_somax_seq[index], axis=0, keepdims=True)])
+
+        # convert to one-hot
+        pred_seq = np.zeros(shape=np.shape(soft_seq))
+        for t in range(np.shape(soft_seq)[1]):
+            max_arg = tf.math.argmax(soft_seq[:, t, :], axis=1)
             y_predi = tf.one_hot(max_arg, depth=len(exp.categories)).numpy()
-            y_predi_seq[:, t, :] = y_predi
+            pred_seq[:, t, :] = y_predi
 
-    return y_evalu, y_predi_seq
+    return true, pred_seq
